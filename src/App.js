807733533144ece23f99b9b83a2f1fb47aa44e27
Gus Richardson <gusrichardson1@gmail.com>
2021-11-18 15:59:59 -0500

    converted form elements to bootstap form elements

--- src/App.js
+++ src/App.js
@@ -1,83 +1,66 @@
-import React, { useState, useEffect } from 'react'
-// import React, { useEffect } from 'react'
-import axios  from 'axios';
-import './App.css';
-import Header from './components/Header';
-import Form from './components/Form';
-import Results from './components/Results'
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import Header from "./components/Header";
+import SearchForm from "./components/SearchForm";
+import Results from "./components/Results";
 
 function App() {
-
   let [stock, setStock] = useState([]);
   let [ticker, setTicker] = useState("");
   let [region, setRegion] = useState("");
 
-   const options = {
+  const options = {
     method: "GET",
     url: "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete",
     params: {
       q: ticker,
-      region: region
+      region: region,
     },
     headers: {
       "x-rapidapi-key": "ade8220407msh0441b9e7214b5dfp1ec580jsnd9c4e9d4905a",
-      'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
-    }
+      "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+    },
   };
-
-  // useEffect(() => {
-  //   axios.request(options).then((res) => {
-  //     console.log(res.data);
-  //     setStocks(res.data.news);
-  //   }).catch((err) => {
-  //     console.error(err);
-  //   });
-  // }, [])
 
   const handleInput = (input) => {
     setTicker(input);
     console.log(ticker);
-  }
+  };
 
-    const handleRegion = (input) => {
+  const handleRegion = (input) => {
     setRegion(input);
     console.log(region);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-      axios.request(options).then((res) => {
-      console.log(res.data);
-      let stocksArr = [...stock, res.data];
-      // setStock(res.data);
-      setStock(stocksArr);
-    }).catch((err) => {
-      console.error(err);
-    });
-  }
-
+    console.log("OPTIONS", options);
+    axios
+      .request(options)
+      .then((res) => {
+        console.log(res.data);
+        let stocksArr = [...stock, res.data];
+        setStock(stocksArr);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
     <div className="App">
-     <Header title="Yahoo Finance App" />
-     <Form handleInput={handleInput} handleRegion={handleRegion} handleSubmit={handleSubmit}/>
-     {/* {
-       stock.quotes ? <Stock quotes={stock.quotes} news={stock.news} /> : <p>Enter a stock and a region in the form above</p> 
-     } */}
-
-      {
-        stock.length > 0 ? <Results stocks={stock} /> : <p>Enter a stock and a region in the form above</p>
-      }
-
-      {/* {
-        stocks.map(stock => {
-          return <Stock
-                    key={stock.uuid}
-                    title={stock.title}
-                    link={stock.link}
-                       />
-        })
-      } */}
+      <Header title="Yahoo Finance App" />
+      <SearchForm
+        handleInput={handleInput}
+        handleRegion={handleRegion}
+        handleSubmit={handleSubmit}
+      />
+      {stock.length > 0 ? (
+        <Results stocks={stock} />
+      ) : (
+        <p>Enter a stock and a region in the form above</p>
+      )}
     </div>
   );
 }
---
+++ src/components/SearchForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { InputGroup, FormControl, Form } from "react-bootstrap";
+
+function SearchForm({ handleInput, handleRegion, handleSubmit }) {
+  return (
+    <form
+      id="stock-form"
+      onSubmit={(e) => {
+        handleSubmit(e);
+      }}
+    >
+      <Form.Label htmlFor="stock-ticker">Enter stock ticker</Form.Label>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="stock-addon">Name</InputGroup.Text>
+        <FormControl
+          placeholder="Enter stock name"
+          aria-label="Enter stock name"
+          aria-describedby="stock-addon"
+          id="stock-ticker"
+          onChange={(e) => handleInput(e.target.value)}
+        />
+      </InputGroup>
+      <Form.Label htmlFor="stock-ticker">Enter company region</Form.Label>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="region-addon">Region</InputGroup.Text>
+        <FormControl
+          placeholder="Enter region"
+          aria-label="Enter region"
+          aria-describedby="region-addon"
+          id="region"
+          onChange={(e) => handleRegion(e.target.value)}
+        />
+      </InputGroup>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+export default SearchForm;
